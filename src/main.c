131ef662266e8c46211db41c5e73e53384b276c9
Paolo Perego <paolo@codiceinsicuro.it>
2022-03-07 09:53:47 +0100

    Import before autoreconf

---
+++ src/main.c
@@ -0,0 +1,23 @@
+/*
+ * chomp: it chomps bytes from its standard input
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
+#include<stdio.h>
+#include"chomp.h"
+
+int main(int argc, char **argv) {
+	return 0;
+}
